7c157068162a3288a11d0357df4ceb106e3d7cac
Annmarie Rodriguez <55721341+AnnmarieJenny@users.noreply.github.com>
2021-02-04 20:30:08 -0500

    class changes

--- js/scripts.js
+++ js/scripts.js
@@ -35,4 +35,50 @@ $(".answer").click(function() {
   if(result=="221"){$("#result-3").show()}
   if(result=="222"){$("#result-3").show()}
   })
-  
+  //data types
+  var someNumber = 5
+  var someString = '5'
+
+  console.log(someNumber + 5)
+  console.log(someString + '5')
+
+// boolean
+
+//var buttonClicked = false
+
+//console.log(buttonClicked)
+
+//$'.boolean-example'.on('click', function() {
+//  buttonClicked = true;
+  // to set buttonClicked to the opposite of what it was
+
+//  if(buttonClicked === true){
+//    console.log('button is on')
+//  } else{
+//    console.log('button is off')
+//  }
+//})
+
+// array
+var myFamily = ['Annmarie', 'Jenny', 'Leo', 'Eric', 'Alexander', 'Irene', 'little-man']
+
+console.log(myFamily[0])
+console.log(myFamily.length)
+
+
+//object
+var myFamilyObject = {
+  mother: 'Jenny',
+  father: 'Leo',
+  brother1: 'Eric',
+  brother2: 'Alexander',
+  sister1: 'Irene',
+  baby1: 'little-man'
+}
+
+console.log('logging my family object', myFamilyObject)
+
+console.log('show me mother', myFamilyObject.mother)
+
+// array of objects
+{}
